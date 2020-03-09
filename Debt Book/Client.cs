bf4079e2f5825e3455c0c2ec19934e2d3b34ac3a
Viktor Lundsgaard Andersen <vik.lund.and@gmail.com>
2020-03-09 10:17:25 +0100

    Add project files.

---
+++ Debt Book/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Debt_Book
+{
+    class Client : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void notify([CallerMemberName]string propname = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propname));
+        }
+    }
+}
