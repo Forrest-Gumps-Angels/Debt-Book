c237753eb4e8c62d62bf6155ccf503ff834d447b
Rasmus Føgh Sørensen <au618865@uni.au.dk>
2020-03-16 14:44:51 +0100

    Added XML serializer to save and open

--- Debt Book/MainWindowViewModel.cs
+++ Debt Book/MainWindowViewModel.cs
@@ -1,8 +1,13 @@
 ﻿using Prism.Mvvm;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.IO.Enumeration;
+using System.Windows.Data;
 using System.Windows.Input;
+using System.Xml.Serialization;
 using Prism.Commands;
 using Debt_Book.ViewModels;
+using Microsoft.Win32;
 
 namespace Debt_Book
 {
@@ -12,6 +17,8 @@ namespace Debt_Book
         Client _currentClient = null;
         DebtHistory.DebtUnit _currentDebtUnit = null;
         private int _currentIndex = 0;
+        string availableFileTypes = "xml files (*.xml)|*.xml";
+        string _filename;
 
         public MainWindowViewModel()
         {
@@ -42,6 +49,12 @@ namespace Debt_Book
         {
             get => _currentIndex;
             set => SetProperty(ref _currentIndex, value);
+        }
+
+        public string FileName
+        {
+            get => _filename;
+            set => SetProperty(ref _filename, value);
         }
 
         ICommand _addDebtor;
@@ -78,6 +91,55 @@ namespace Debt_Book
                     }
                 }));
             }
+        }
+
+        ICommand _saveAs;
+
+        public ICommand SaveAs
+        {
+            get
+            {
+                return _saveAs ?? (_saveAs = new DelegateCommand(() => { executeSaveAs(); }));
+            }
+        }
+
+        public void executeSaveAs()
+        {
+            XmlSerializer XML_serial = new XmlSerializer(typeof(ObservableCollection<Client>));
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = availableFileTypes;
+            saveFileDialog.ShowDialog();
+            FileName = saveFileDialog.FileName;
+
+            TextWriter writer = new StreamWriter(FileName);
+
+            XML_serial.Serialize(writer, ClientList);
+            writer.Close();
+        }
+
+        ICommand _open;
+
+        public ICommand Open
+        {
+            get
+            {
+                return _open ??  (_open = new DelegateCommand(() =>
+                {
+                    executeOpen();
+
+                }));
+            }
+        }
+
+        public void executeOpen()
+        {
+            XmlSerializer XML_serial = new XmlSerializer(typeof(ObservableCollection<Client>));
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = availableFileTypes;
+            FileName = openFileDialog.FileName;
+            FileStream fs = new FileStream(FileName, FileMode.Open);
+            ClientList_ = (ObservableCollection<Client>)XML_serial.Deserialize(fs);
+            fs.Close();
         }
     }
 }
