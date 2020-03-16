2c4c05acd993032937431f0024cd1a9373513bdd
Rasmus Føgh Sørensen <au618865@uni.au.dk>
2020-03-16 13:02:34 +0100

    Modified Client

--- Debt Book/Client.cs
+++ Debt Book/Client.cs
@@ -8,12 +8,36 @@ namespace Debt_Book
 {
     class Client : INotifyPropertyChanged
     {
-
+        string name;
+        double initialValue;
+        double accumulatedDebt;
 
         public event PropertyChangedEventHandler PropertyChanged;
+
         protected void notify([CallerMemberName]string propname = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propname));
+        }
+
+        public Client()
+        { }
+
+        public Client(string _name, double _initialValue)
+        {
+            name = _name;
+            initialValue = _initialValue;
+        }
+
+        public string Name
+        {
+            get => name;
+            set { name = value; notify("Name"); }
+        }
+
+        public double InitialValue
+        {
+            get => initialValue;
+            set { initialValue = value; notify("InitialValue"); }
         }
     }
 }
